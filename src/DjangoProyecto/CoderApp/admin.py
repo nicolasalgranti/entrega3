1ef3d727324b78de510685a33aa9afdf890edef6
nicolasalgranti <nicolasalgranti@gmail.com>
2024-01-25 22:18:18 -0300

    entrega.final

--- src/DjangoProyecto/CoderApp/admin.py
+++ src/DjangoProyecto/CoderApp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+
+from CoderApp.models import (
+    Sugerencias,
+    Staff,
+    StaffSolicitud,
+    Avatar
+)
+
+admin.site.register(Sugerencias)
+admin.site.register(Staff)
+admin.site.register(StaffSolicitud)
+admin.site.register(Avatar)
+
 # Register your models here.
